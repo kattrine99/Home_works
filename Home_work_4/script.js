e18bf021ce170c7e1ac5ca22b95b432a94d259f2
Ekaterina <kattren990703@gmail.com>
2024-09-14 15:48:21 +0500

    Homework#4 конвертер валют

---
+++ Home_work_4/script.js
@@ -0,0 +1,29 @@
+let moneyType = document.querySelector("#MoneyType");
+let Currency = document.getElementById("cur");
+let UZbMoney = document.getElementById("uzs");
+
+moneyType.addEventListener("input", convertMoney)
+function convertMoney(){
+    let MoneyValue = document.getElementsByName("currency")[0].value;
+    let currentMoneyType = moneyType.value;
+    let Dollar = 12700;
+    let Rubl = 133;
+    let Euro = 13920;
+    let money;
+
+    if(currentMoneyType =="USD"){
+        money = Dollar;
+        Currency.innerHTML = "UZD";
+    }
+    else if (currentMoneyType =="RUB"){
+        money = Rubl;
+        Currency.innerHTML = "RUB";
+    }
+    else{
+        money = Euro;
+        Currency.innerHTML = "EUR"
+        ;
+    }
+    let total = money * MoneyValue;
+    UZbMoney.innerHTML = "UZS: " + total;
+}
